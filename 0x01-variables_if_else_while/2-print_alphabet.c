7b1c687b57ec793093f4b2c916d72407e5876539
wachira141 <wachiraw75@gmail.com>
2022-07-01 09:19:08 -0700

    2-print-alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+/**
+* main - Program entry point
+*
+* Return: 0 indicates
+*/
+int main(void)
+{
+char alphabet;
+/* Program */
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+putchar(alphabet)
+putchar('\n');
+return (0);
+}
