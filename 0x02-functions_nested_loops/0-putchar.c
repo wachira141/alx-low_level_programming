a25e45d3d8f8abab29fd625e041cca80a272556e
wachira141 <wachiraw75@gmail.com>
2022-07-05 11:38:55 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,14 @@
 */
 int main(void)
 {
-const char str[] = "_putchar\n";
-int len = strlen(str);
-int i;
-for (i = 0; i < len; i++)
-_putchar(str[i]);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
