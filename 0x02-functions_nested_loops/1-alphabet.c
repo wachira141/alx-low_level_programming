deadaf30ecaf35769259d6f8f95f5b653fabce54
wachira141 <wachiraw75@gmail.com>
2022-07-05 12:57:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#nclud "main.h"
+/**
+ * print_alphabet - print lowercase
+ *
+*Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
