9a1448ceb9bff04548e17b71fd018bb131392111
wachira141 <wachiraw75@gmail.com>
2022-07-05 12:34:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,6 +9,7 @@ void print_alphabet_x10(void)
 {
 int count;
 int alphabet;
+count = 0;
 if (count < 10)
 {
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
@@ -18,5 +19,4 @@ _putchar(alphabet);
 count++;
 _putchar('\n');
 }
-return (0);
 }
