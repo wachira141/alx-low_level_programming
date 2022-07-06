e8158443d23c7904f7847c856c6188f28b19eabf
wachira141 <wachiraw75@gmail.com>
2022-07-06 06:33:51 -0700

    updated main.h file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,5 +2,12 @@
 #define MAIN_H
 #include <stdio.h>
 void positive_or_negative(int i);
+/**
+* largest_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
 int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
