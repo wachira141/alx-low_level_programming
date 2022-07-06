cfef415d96511ca334aae717f7e4395fec53dfe4
wachira141 <wachiraw75@gmail.com>
2022-07-06 05:31:01 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - function positive_or_negative
+* Description - positive or negative
+* Return: 0
+*/
+int positive_or_negative(void)
+{
+int i;
+if (i > 0)
+printf('%d is positive\n', i);
+else if (i < 0)
+printf('%d is negative\n', i);
+else
+printf('%d is 0\n', i);
+return (0);
+}
