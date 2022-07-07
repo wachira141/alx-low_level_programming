caa4bcf8ccfcfef19c60e5f8c92eafa454f3f1f9
wachira141 <wachiraw75@gmail.com>
2022-07-07 12:51:45 -0700

    issupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _islower - Return 1 if letter is lowercase, 0 if not.
+ * _isupper - Return 1 if letter is lowercase, 0 if not.
  * @c: char type letter
  * Return: 1 if lowercase, 0 if not lowercase
  */
-int _islower(int c)
+int _isupper(int c)
 {
 	if (c > 96 && c < 123)
 		return (1);
