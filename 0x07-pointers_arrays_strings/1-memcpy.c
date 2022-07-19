f6c8085fb61fa4356eb0ae65a3a5cc3d53fd1757
wachira141 <wachiraw75@gmail.com>
2022-07-19 03:19:40 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - append n bytes of memory with a constant value
+ * @dest: destination memory area
+ *  @src: source memory area
+ *  @n: bytes from
+ *  Return: the pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i)  = *(src + i);
+		return (dest);
+	}
