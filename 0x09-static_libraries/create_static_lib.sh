350aa6eb4e5332ef11f611a02f16f81c861c7bf2
wachira141 <wachiraw75@gmail.com>
2022-07-22 16:37:55 -0700

    create_stlib

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-# compile
-
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
-
-# create lib
-
-ar cr liball.a *.o
+!/bin/bash
+gcc -c -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
+ar -rc liball.a *.o
