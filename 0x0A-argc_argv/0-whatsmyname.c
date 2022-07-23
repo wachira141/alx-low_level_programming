d591ad9db357bc6af587bf7014fd627dff291659
wachira141 <wachiraw75@gmail.com>
2022-07-23 05:30:17 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc - array length
+ * @argv - arguments passed
+ *
+ * Return -Always 0 (Success)
+ */
+int main(int argc, char *argv[]){
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,4 @@
+#ifndef _MAIN_H
+#define _MAIN_
+int _putchar(char c);
+#endif
