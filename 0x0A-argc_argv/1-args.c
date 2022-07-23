db3600937ef57df5ae0bb3d7d1a1867214a3d66d
wachira141 <wachiraw75@gmail.com>
2022-07-23 06:59:19 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * main - Prints numebr of arguments passed to it
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
