85ab1129c39fc9b496062de0a140dc2bfc6766bf
wachira141 <wachiraw75@gmail.com>
2022-07-28 10:21:20 -0700

    string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,18 +19,18 @@ s1 = "";
 if (!s2)
 	s2 = "";
 
-if (n >= strlen(s2))
+if (n >= (int) strlen(s2))
 	len2 = strlen(s2);
 
 	newstr = malloc(sizeof(*newstr) * (strlen(s1) + len2 + 1));
 if (!newstr)
 	return (NULL);
 
-for (i = 0; i <= strlen(s1); i++)
+for (i = 0; i <= s1[i] != '\0'; i++)
 	newstr[i] = s1[i];
 
 for (j = 0; j <= len2; j++)
-	newstr[i + j + 1] = s2[j];
+	newstr[i + j] = s2[j];
 
 newstr[i + j] = '\0';
 }
