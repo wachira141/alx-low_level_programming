168c8a31f93a6135b91fe81a7802bc6c24b33e1e
wachira141 <wachiraw75@gmail.com>
2022-07-28 09:28:15 -0700

    string concantination

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * string_nconcat - function to concat 2 characters
+ * @s1: the first string
+ * @s2: the second string
+ *@n: the number of int
+ * Return: return pointer to newly alloc space
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *newstr;
+int i, j, len2;
+
+len2 = n;
+if (!s1)
+s1 = "";
+if (!s2)
+	s2 = "";
+
+if (n >= strlen(s2))
+	len2 = strlen(s2);
+
+	newstr = malloc(sizeof(*newstr) * (strlen(s1) + len2 + 1));
+if (!newstr)
+	return (NULL);
+
+for (i = 0; i <= strlen(s1); i++)
+	newstr[i] = s1[i];
+
+for (j = 0; j <= len2; j++)
+	newstr[i] = s2[j];
+
+newstr[i + j] = '\o';
+}
