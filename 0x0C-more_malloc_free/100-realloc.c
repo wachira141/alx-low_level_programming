0a22932f04708ee84ffbcb33f5a5a01f9cc631aa
wachira141 <wachiraw75@gmail.com>
2022-07-29 07:39:38 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,23 +13,23 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		free(ptr);
 		ptr = malloc(new_size);
-		return(ptr);
+		return (ptr);
 	}
-	if (new_size == old-size)
-		return(ptr);
+	if (new_size == old_size)
+		return (ptr);
 
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
-			return(ptr);
+			return (ptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return(NULL);
+		return (NULL);
 	}
 	free(ptr);
-		return(ptr);
+		return (ptr);
 
 
 }
