13755e462329b3aecf4366472e62c48f88216a34
wachira141 <wachiraw75@gmail.com>
2022-07-29 07:35:04 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,4 +5,31 @@
  * @old_size: prev memory allocated
  * @new_size: new mem block
  *
- * Return: return to 
+ * Return: return pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size > old_size)
+	{
+		free(ptr);
+		ptr = malloc(new_size);
+		return(ptr);
+	}
+	if (new_size == old-size)
+		return(ptr);
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+			return(ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return(NULL);
+	}
+	free(ptr);
+		return(ptr);
+
+
+}
