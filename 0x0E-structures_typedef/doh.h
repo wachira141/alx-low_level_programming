aa7a690d99c0067850771eb9c2ac1d837f47bb8c
wachira141 <wachiraw75@gmail.com>
2022-08-01 04:03:02 -0700

    dog struct

---
+++ 0x0E-structures_typedef/doh.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/*
+ * struct dog - the strut to define a dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: this is a dog structure
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif
