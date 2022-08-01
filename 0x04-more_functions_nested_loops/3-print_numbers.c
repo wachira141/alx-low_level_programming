0bcfa4ec03b64163c9c173b6e53dc60b48f93ce1
wachira141 <wachiraw75@gmail.com>
2022-08-01 09:03:30 -0700

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_numbers - fn to print nmbers
+ * Return: return 0
+ */
+void print_numbers(void)
+{
+int i = 0;
+while (i <= 9)
+	_putchar(i);
+_putchar("\n");
+}
