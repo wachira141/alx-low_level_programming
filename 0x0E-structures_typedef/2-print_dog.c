7c942b8dca7bf6978e9a54d3797ca7780a230c5d
wachira141 <wachiraw75@gmail.com>
2022-08-01 06:21:51 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ if (d != NULL)
 {
 	printf("Name: ");
 	d->name == NULL ? printf("null\n") : printf("%s\n", d->name);
+	printf("Age: %f\n", d->age);
 	printf("Owner: ");
 	d->owner == NULL ? printf("null\n") : printf("%s\n", d->owner);
-printf("Age: %f\n", d->age);
 }
 }
