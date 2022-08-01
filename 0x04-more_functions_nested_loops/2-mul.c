f4aece12a33d7fecdf3ba7aef615460270407399
wachira141 <wachiraw75@gmail.com>
2022-08-01 08:56:47 -0700

    multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * mul - function to multiply
+ * @a: first integer
+ * @b: second integer
+ *Return: return multiple
+ */
+int mul(int a, int b)
+{
+int c;
+
+c = a * b;
+return (c);
+}
