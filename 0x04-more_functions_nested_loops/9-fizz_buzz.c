400c7c55a849a2d904f408ba4bb67f64fde907b6
wachira141 <wachiraw75@gmail.com>
2022-08-02 07:32:38 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-int i = 0;
+int i = 1;
 
 while (i <= 100)
 {
 	if (i % 3 == 0 && i % 5 == 0)
 		printf("FizzBuzz");
-	if (i % 3 == 0)
+	else if (i % 3 == 0)
 		printf("Fizz");
 
-	if (i % 5 == 0)
+	else if (i % 5 == 0)
 		printf("Buzz");
 
 	else
