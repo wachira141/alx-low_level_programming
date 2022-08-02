b30841766d8aae0504e58fb3afda704d20ff7e3b
wachira141 <wachiraw75@gmail.com>
2022-08-02 08:27:53 -0700

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,38 +8,16 @@
  */
 void print_number(int n)
 {
-long m;
-int c;
-long num;
+	unsigned int num = n;
 
-num = n;
-
-if (num < 0)
-{
-	num *= -1;
-	putchar('-');
-}
-m = 1;
-c = 1;
-
-while (c)
-{
-	if (num / (m * 10) > 0)
-		m *= 10;
-	else
-		c = 0;
-}
-while (num >= 0)
-{
-	if (m == 1)
+	if (n < 0)
 	{
-		putchar(num % 10 + '0');
-		num = -1;
+		putchar('-');
+		num = -num;
 	}
-	else
-	{
-		putchar((num / m % 10) + '0');
-		m /= 10;
-	}
-}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	putchar((num % 10) + '0');
 }
