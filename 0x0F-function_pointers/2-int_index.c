e9facf92825895e308bb4d144b8c7a5ea2526b7a
wachira141 <wachiraw75@gmail.com>
2022-08-03 04:38:39 -0700

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,18 +8,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int *temp;
 int i;
 
-if (size <= 0)
+if (size < 1 || array == NULL || cmp == NULL)
 	return (-1);
 
 for (i = 0; i < size; i++)
 {
-temp = cmp(array[i]);
-if (*temp != 0)
-	return (i);
-if (*temp == 0)
-	return (-1);
+	if (cmp(array[i]))
+		return (i);
 }
+return (-1);
 }
