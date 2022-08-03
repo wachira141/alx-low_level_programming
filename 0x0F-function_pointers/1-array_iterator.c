6a19a5ffdb6ca0014d0aabac2d5dce7d6fd67b75
wachira141 <wachiraw75@gmail.com>
2022-08-03 03:45:45 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointer.h"
+/**
+ * array_iterator - func to iterate
+ * @array: the arr passed as param
+ * @size: size of the array
+ * @action: pointer to func to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+
+for (i = 0; i < size; i++)
+	action(array[i]);
+}
