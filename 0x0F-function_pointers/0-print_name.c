11e03def910b9981ef75ec65d2ecbbbe2ac3d9f0
wachira141 <wachiraw75@gmail.com>
2022-08-03 03:09:27 -0700

    whats my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - func to print name
+ * @name: name to be printed
+ * @f: pointer func as param
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
