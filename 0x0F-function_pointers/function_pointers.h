2eff8ecb2e6a7e7cb054e9e8364e0e5d879bfa89
wachira141 <wachiraw75@gmail.com>
2022-08-03 03:26:57 -0700

    print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTER_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 #include <stdio.h>
 #include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
