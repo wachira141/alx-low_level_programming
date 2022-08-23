58f01bd40c72ee1c4d5988685569b688f6e3f5a7
wachira141 <wachiraw75@gmail.com>
2022-08-23 04:14:29 -0700

    task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
+#define __home inline
+#define __silent __attribute__((unused))
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
