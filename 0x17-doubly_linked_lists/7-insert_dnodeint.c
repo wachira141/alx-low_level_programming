6bb329ccec928fe51c5e2c4439a84b97375a79ac
wachira141 <wachiraw75@gmail.com>
2022-09-29 04:02:36 -0700

    code errors rectified

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,54 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at specific index
- * @h: head of linked list
- * @idx: index of new node
- * @n: new node value
- * Return: inserted node
+ * insert_node - insert node at given index
+ * @tmp: ptr to nth position node in doubly linked list
+ * @n: node data
+ * Return: address of inserted node
  */
-
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_node(dlistint_t *tmp, int n)
 {
-	dlistint_t *current;
 	dlistint_t *new;
 
-	if (h == NULL)
-		return (0);
-
-	current = *h;
-
-	while (idx != 0)
-	{
-		current = current->next;
-		idx--;
-		if (current == NULL)
-			return (NULL);
-	}
-
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-	{
-		free(new);
+	new = malloc(sizeof(struct dlistint_s));
+	if (!new)
 		return (NULL);
+	new->n = n;
+
+	new->next = tmp;
+	new->prev = tmp->prev;
+	tmp->prev->next = new;
+	tmp->prev = new;
+
+	return (new);
+}
+
+/**
+ * insert_dnodeint_at_index - create and insert node at nth index
+ * @h: pointer to head of list
+ * @idx: index
+ * @n: node data
+ * Return: address of inserted node, or NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (!h)
+		return (NULL);
+
+	tmp = *h;
+	while ((idx != 0) && (tmp->next))
+	{
+		idx -= 1;
+		tmp = tmp->next;
+		if (idx == 0)
+			return (insert_node(tmp, n));
 	}
 
-	new->n = n;
-	new->next = current;
-	new->prev = current->prev;
-	if (current->prev != NULL)
-		current->prev->next = new;
-
-	/*TODO: Handle special case when idx is 0 and last index*/
-
-	return (current);
+	if (idx == 1)
+		return (add_dnodeint_end(h, n));
+	return (NULL);
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,45 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - delete node at specific index
- * @head: head of linked list
- * @index: index of node to delete
- * Return: 1 on success, -1 on failure
+ * delete_dnodeint_at_index - delete a node at a given position
+ * @head: pointer to head pointer of double linked list
+ * @index: index to delete node
+ * Return: 1 if succeeded, or -1 if failed
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
 
-	if (head == NULL)
+	dlistint_t *tmp;
+
+	if (*head == NULL)
 		return (-1);
 
-	current = *head;
+	tmp = *head;
 
-	if (current == NULL)
-		return (-1);
-
-	while (index != 0)
+	if (index == 0)
 	{
-		current = current->next;
-		index--;
-		if (current == NULL)
-			return (-1);
+		*head = tmp->next;
+		free(tmp);
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		return (1);
 	}
 
-	if (current->prev != NULL)
-		current->prev->next = current->next;
-	else
+	while ((index != 0) && (tmp->next != NULL))
 	{
-		*head = current->next;
-		if (current->next != NULL)
-			current->next->prev = NULL;
-		free(current);
+		index -= 1;
+		tmp = tmp->next;
 	}
 
-	return (1);
+	if (index == 0)
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+		free(tmp);
+		return (1);
+	}
+
+	return (-1);
 }
